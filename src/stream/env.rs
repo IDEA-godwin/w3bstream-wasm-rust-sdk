6e74af8e0820fca34d80f65b86fc099c381698ef
Godwin Igwurube <godwinaquinas@gmail.com>
2023-03-08 00:36:11 +0100

    change get_env retrun to option

--- src/stream/env.rs
+++ src/stream/env.rs
@@ -1,19 +1,18 @@
 use super::super::host::abi::*;
-use anyhow::{bail, Result};
 
 /// Retrieves the environment variables of the project by the key.
 ///
 /// # Examples
 ///
 /// ```no_run
-/// use ws-sdk::stream
-/// let env = get_env("key")?;
+/// use ws-sdk::stream::get_env;
+/// let env = get_env("key").unwrap_or("default_value");
 /// ```
-pub fn get_env(key: &str) -> Result<String> {
+pub fn get_env(key: &str) -> Option<String> {
     let data_ptr = &mut (0 as i32) as *const _ as *const *mut u8;
     let data_size = &mut (0 as i32) as *const i32;
     match unsafe { ws_get_env(key.as_bytes().as_ptr(), key.len() as _, data_ptr, data_size) } {
-        0 => Ok(unsafe { String::from_raw_parts(*data_ptr, *data_size as _, *data_size as _) }),
-        _ => bail!("fail to get env"),
+        0 => Some(unsafe { String::from_raw_parts(*data_ptr, *data_size as _, *data_size as _) }),
+        _ => None,
     }
 }
