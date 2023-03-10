4a704901564c3edfdad7983fa2341577fe6e8245
Godwin Igwurube <godwinaquinas@gmail.com>
2023-03-10 01:32:27 +0100

    remove return from log

--- src/log/log.rs
+++ src/log/log.rs
@@ -16,13 +16,10 @@ enum LogLevel {
 ///
 /// ```no_run
 /// use ws-sdk::log::log_info
-/// log_info("hello world!")?;
+/// log_info("hello world!");
 /// ```
-pub fn log_info(str: &str) -> Result<()> {
-    match unsafe { ws_log(LogLevel::Info as _, str.as_bytes().as_ptr(), str.len() as _) } {
-        0 => Ok(()),
-        _ => bail!("fail to log the info"),
-    }
+pub fn log_info(str: &str) {
+    unsafe { ws_log(LogLevel::Info as _, str.as_bytes().as_ptr(), str.len() as _) };
 }
 
 /// log an error message.
@@ -31,17 +28,14 @@ pub fn log_info(str: &str) -> Result<()> {
 ///
 /// ```no_run
 /// use ws-sdk::log::log_error
-/// log_error("error!")?;
+/// log_error("error!");
 /// ```
-pub fn log_error(str: &str) -> Result<()> {
-    match unsafe {
+pub fn log_error(str: &str) {
+    unsafe {
         ws_log(
             LogLevel::Error as _,
             str.as_bytes().as_ptr(),
             str.len() as _,
         )
-    } {
-        0 => Ok(()),
-        _ => bail!("fail to log the error"),
-    }
+    };
 }
